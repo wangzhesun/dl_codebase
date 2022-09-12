d4a7c4a17e79f8ec6fdaaf66a076e4c6013f942a
wangzhesun <wangzhe.sun@outlook.com>
2022-09-12 00:53:36 -0500

    Update GIFS_seg_trainer.py

--- modules/trainer/GIFS_seg_trainer.py
+++ modules/trainer/GIFS_seg_trainer.py
@@ -23,6 +23,23 @@ class GIFS_seg_trainer(seg_trainer):
         self.continual_aug_train_set = dataset_module.get_continual_aug_train_set(cfg)
         self.continual_test_set = dataset_module.get_continual_test_set(cfg)
 
+
+
+        #########################################################################
+        print('\n printing max and min index 0 and 1 test image: ')
+        print(self.continual_aug_train_set[0][0].size())
+        print(self.continual_aug_train_set[1][0].size())
+        print(torch.max(self.continual_aug_train_set[0][0]))
+        print(torch.min(self.continual_aug_train_set[0][0]))
+        print(self.continual_aug_train_set[0][0].is_cuda)
+        print('\n printing max and min index 0 and 1 test label: ')
+        print(self.continual_aug_train_set[0][1].size())
+        print(self.continual_aug_train_set[1][1].size())
+        print(torch.max(self.continual_aug_train_set[0][1]))
+        print(torch.min(self.continual_aug_train_set[0][1]))
+        print(self.continual_aug_train_set[0][1].is_cuda)
+        #########################################################################
+
         self.continual_test_loader = torch.utils.data.DataLoader(self.continual_test_set,
                                                                  batch_size=cfg.TEST.batch_size,
                                                                  shuffle=False,
