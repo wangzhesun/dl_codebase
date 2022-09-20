2f79260c2047ef26babd3781bad97c1e80f2e8e8
wangzhesun <wangzhe.sun@outlook.com>
2022-09-20 00:04:27 -0500

    Update voc_5shot_eval.sh

--- voc_5shot_eval.sh
+++ voc_5shot_eval.sh
@@ -3,12 +3,12 @@
 #python3 main/test.py --cfg configs/fs_incremental/pascal5i_split2_5shot.yaml --load /data/cvpr2022/pascal5i_split2_BR_BS32.pt
 #python3 main/test.py --cfg configs/fs_incremental/pascal5i_split3_5shot.yaml --load /data/cvpr2022/pascal5i_split3_BR_BS32.pt
 
-python main/test.py --cfg configs/non_fs_incremental/voc2012_5i_split3.yaml --load voc2012_split3_non_fs_final.pt
+#python main/test.py --cfg configs/non_fs_incremental/voc2012_5i_split3.yaml --load voc2012_split3_non_fs_final.pt
 
 
 
 ## few shot split 0
-#python main/test.py --cfg configs/fs_incremental/voc2012_5i_fs_split0_5shot.yaml --load voc2012_split0_non_fs_final.pt
+python main/test.py --cfg configs/fs_incremental/voc2012_5i_fs_split0_5shot.yaml --load voc2012_split0_non_fs_final.pt
 #
 ## few shot split 1
 #python main/test.py --cfg configs/fs_incremental/voc2012_5i_fs_split1_5shot.yaml --load voc2012_split0_non_fs_final.pt
