da16cdf888ed9a47794b9537ba9c625395f31eca
wangzhesun <wangzhe.sun@outlook.com>
2022-09-19 23:15:45 -0500

    Update voc_5shot_eval.sh

--- voc_5shot_eval.sh
+++ voc_5shot_eval.sh
@@ -1,4 +1,6 @@
-python3 main/test.py --cfg configs/fs_incremental/pascal5i_split0_5shot.yaml --load /data/cvpr2022/pascal5i_split0_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/pascal5i_split1_5shot.yaml --load /data/cvpr2022/pascal5i_split1_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/pascal5i_split2_5shot.yaml --load /data/cvpr2022/pascal5i_split2_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/pascal5i_split3_5shot.yaml --load /data/cvpr2022/pascal5i_split3_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/pascal5i_split0_5shot.yaml --load /data/cvpr2022/pascal5i_split0_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/pascal5i_split1_5shot.yaml --load /data/cvpr2022/pascal5i_split1_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/pascal5i_split2_5shot.yaml --load /data/cvpr2022/pascal5i_split2_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/pascal5i_split3_5shot.yaml --load /data/cvpr2022/pascal5i_split3_BR_BS32.pt
+
+python main/test.py --cfg configs/non_fs_incremental/voc2012_5i_split3.yaml --load voc2012_split3_non_fs_final.pt
