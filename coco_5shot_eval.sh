995b8f88f036c1ba85791e3c74edf5790d3d1586
wangzhesun <wangzhe.sun@outlook.com>
2022-09-20 23:44:43 -0500

    update code

--- coco_5shot_eval.sh
+++ coco_5shot_eval.sh
@@ -1,4 +1,17 @@
-python3 main/test.py --cfg configs/fs_incremental/coco_20i_split0_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split0_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/coco_20i_split1_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/coco_20i_split2_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split2_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/coco_20i_split3_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split3_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/coco_20i_split0_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split0_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/coco_20i_split1_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/coco_20i_split2_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split2_BR_BS32.pt
+#python3 main/test.py --cfg configs/fs_incremental/coco_20i_split3_5shot_renorm.yaml --load /data/cvpr2022/coco20i_split3_BR_BS32.pt
+
+
+## few shot split 0
+#python main/test.py --cfg configs/fs_incremental/coco20i_split0_5shot.yaml --load GIFS_coco20i_split0_final.pt
+#
+## few shot split 1
+#python main/test.py --cfg configs/fs_incremental/coco20i_split1_5shot.yaml --load GIFS_coco20i_split1_final.pt
+#
+## few shot split 2
+#python main/test.py --cfg configs/fs_incremental/coco20i_split2_5shot.yaml --load GIFS_coco20i_split2_final.pt
+#
+## few shot split 3
+python main/test.py --cfg configs/fs_incremental/coco20i_split3_5shot.yaml --load GIFS_coco20i_split3_final.pt
