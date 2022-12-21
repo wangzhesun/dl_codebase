2303982c0977b4c51aa394595ca0c6221ec10ffd
wangzhesun <70256515+wangzhesun@users.noreply.github.com>
2022-12-21 04:59:52 -0800

    Update coco_5shot_eval.sh

--- coco_5shot_eval.sh
+++ coco_5shot_eval.sh
@@ -5,10 +5,10 @@
 
 
 ## few shot split 0
-python main/test.py --cfg configs/fs_incremental/coco20i_split0_5shot.yaml --load GIFS_coco20i_split0_final.pt
+# python main/test.py --cfg configs/fs_incremental/coco20i_split0_5shot.yaml --load GIFS_coco20i_split0_final.pt
 #
 ## few shot split 1
-#python main/test.py --cfg configs/fs_incremental/coco20i_split1_5shot.yaml --load GIFS_coco20i_split1_final.pt
+python main/test.py --cfg configs/fs_incremental/coco20i_split1_5shot.yaml --load GIFS_coco20i_split1_final.pt
 #
 ## few shot split 2
 #python main/test.py --cfg configs/fs_incremental/coco20i_split2_5shot.yaml --load GIFS_coco20i_split2_final.pt
